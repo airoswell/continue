f237430d8e0fb61be028b67e305a85fbbc6582b6
airoswell <ai.roswell@gmail.com>
2015-01-11 20:38:37 -0500

    1.4

--- app/GenericAPI.py
+++ app/GenericAPI.py
@@ -78,7 +78,7 @@ class XDetailAPIView(APIView):
             # only the object that is owned by the user can be updated
             self.model._meta.get_field("owner")
             instance, errors = crud.update(data, owner=request.user)
-            return Response(data=errors)
+            return Response(data=self.serializer(instance).data)
         except FieldDoesNotExist:
             # If the model does not have 'owner' field
             # pass in <user> to the model methods, let them decide
